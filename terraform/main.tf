7c51b0c12fb5b87d71907eb82f32ac47f1fc08b3
Pavlenko Oleksii <apavlenko005@gmail.com>
2024-11-02 18:18:04 +0200

    conncted to aws thru extension n added new ami key

--- terraform/main.tf
+++ terraform/main.tf
@@ -10,12 +10,13 @@ terraform {
 
 # Configure the AWS provider
 provider "aws" {
-  region     = "eu-central-1"
+  region = "us-east-1"
 }
 
 resource "aws_security_group" "web_app" {
   name        = "web_app"
-  description = "security group"
+  description = "security group for web application"
+
   ingress {
     from_port   = 80
     to_port     = 80
@@ -23,28 +24,30 @@ resource "aws_security_group" "web_app" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
- ingress {
+  ingress {
     from_port   = 22
     to_port     = 22
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
+
   egress {
     from_port   = 0
-    to_port     = 65535
-    protocol    = "tcp"
+    to_port     = 0
+    protocol    = "-1"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
-  tags= {
+  tags = {
     Name = "web_app"
   }
 }
 
 resource "aws_instance" "webapp_instance" {
-  ami           = "ami-0669b163befffbdfc"
+  ami           = "ami-06b21ccaeff8cd686"
   instance_type = "t2.micro"
-  security_groups= ["web_app"]
+  vpc_security_group_ids = [aws_security_group.web_app.id]
+
   tags = {
     Name = "webapp_instance"
   }
