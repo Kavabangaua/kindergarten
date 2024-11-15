eb67eb3acb7044925519343f81328161236efd53
Pavlenko Oleksii <apavlenko005@gmail.com>
2024-11-16 00:14:11 +0200

    added backend config to tf

--- terraform/main.tf
+++ terraform/main.tf
@@ -6,6 +6,13 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket         = "kavabanga-tf-state-bucket"
+    key            = "terraform.tfstate"
+    region         = "us-east-1"
+    dynamodb_table = "kavabanga-tf-lockid"
+  }
 }
 
 # Configure the AWS provider
